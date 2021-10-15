8933e4c0cf72522badc3361c1f80fdd8bca695a1
Binh Trinh An (VN) <Binh.TrinhA@homecredit.vn>
2021-10-15 15:34:17 +0700

    add post request

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # main.py
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Iterator, Optional
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 @app.get("/")
 def hello():
     return {"message":"Wellcome to Binh.TrinhA Fast API demo"}
+
+@app.post('/vnpost')
+async def create_item(item: Item):
+    return item
